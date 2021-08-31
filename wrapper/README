7edda7c38f631cee2ff96dc541a96cd2724a2945
anon-sca <amha@amha.anonaddy.com>
2021-08-31 09:58:50 +0200

    Add wrapper and cmd files

---
+++ wrapper/README
@@ -0,0 +1,4 @@
+# Requirements for compilation:
+
+C library for Broadcom BCM 2835 as used in Raspberry Pi need to be installed
+https://www.airspayce.com/mikem/bcm2835/
---
+++ wrapper/wrapper.c
@@ -0,0 +1,56 @@
+/****
+  gcc wrapper.c -o wrapper -l bcm2835 -l pthread
+ *****/
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <bcm2835.h>
+#include <pthread.h>
+
+#define PIN RPI_GPIO_P1_11
+pthread_t t;
+
+void *thread (void *arg) 
+{
+	char *cmd = arg; 	
+	//printf("Executing ==> %s\n", cmd);
+	bcm2835_gpio_write(PIN, HIGH);
+	system(cmd);
+}
+
+void microsleep(int microseconds)  
+{
+	long usec = microseconds % 1000000;
+	struct timespec ts_sleep;
+	ts_sleep.tv_sec = microseconds / 1000000;
+	ts_sleep.tv_nsec = 1000*usec;
+	nanosleep(&ts_sleep, NULL);
+}
+
+int main(int argc, char *argv[]) 
+{
+	if (argc < 2) {
+		printf("usage: %s cmd [duration] (us)\n", argv[0]);
+		return 1;
+	}
+	if (!bcm2835_init()) {
+		printf("error\n");
+		return 1;
+	}
+	char *cmd = argv[1];
+	bcm2835_gpio_fsel(PIN, BCM2835_GPIO_FSEL_OUTP);
+	if(argc > 2) {
+		pthread_create(&t, NULL, &thread, cmd);
+		int duration = atoi(argv[2]);
+		microsleep(duration);
+		pthread_cancel(t);
+	}
+	else {
+		bcm2835_gpio_write(PIN, HIGH);
+		system(cmd);
+	}
+	bcm2835_gpio_write(PIN, LOW);
+	bcm2835_close();
+	return 0;
+}
+
